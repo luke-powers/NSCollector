6f4017399d0dd51d24b7d71e1b16c2ad68cf3adc
Luke Powers <luke@openx.com>
2018-03-26 15:00:59 -0700

    updated installers

--- setup.py
+++ setup.py
@@ -3,10 +3,30 @@ from setuptools import setup, find_packages
 from setuptools.command.test import test
 import sys
 
-BASE_DIR = os.path.dirname(__file__)
-PACKAGE_NAME = 'NSCollector'
-SRC_DIR = os.path.join(BASE_DIR, "src", PACKAGE_NAME.lower())
 
+def _get_src_dir(package_name=None):
+    '''Get the source directory by looking in src, or if package_name is
+    given, return that combined with the src directory.
+
+    '''
+    ret = ''
+    if 'PY_PACKAGE_NAME' in os.environ:
+        ret = os.path.join(BASE_DIR, 'src', os.environ['PY_PACKAGE_NAME'])
+    elif package_name:
+        ret = os.path.join(BASE_DIR, 'src', package_name)
+    if not ret:
+        dirs_with_about = [dir for dir in os.listdir('src')
+                           if '__about__.py' in os.listdir('src/'+dir)]
+        if len(dirs_with_about) > 1:
+            print 'More than one source with __about__.py defined, set PY_PACKAGE_NAME and re-run.'
+            exit
+        else:
+            ret = os.path.join(BASE_DIR, 'src', dirs_with_about.pop())
+    return ret
+
+
+BASE_DIR = os.path.dirname(__file__)
+SRC_DIR = _get_src_dir()
 about = {}
 with open(os.path.join(SRC_DIR, "__about__.py")) as f:
     exec(f.read(), about)
@@ -27,29 +47,21 @@ class PyTest(test):
 
 setup(
     author=about['__author__'],
+    author_email=about['__author_email__'],
     classifiers=about['__classifiers__'],
     cmdclass={'test': PyTest},
-    description=PACKAGE_NAME.replace('_', ' ').title(),
-    entry_points={
-        'console_scripts':
-        [
-            '%(pkg_name_cli)s=%(pkg_name)s.__main__:main'
-            % {
-                'pkg_name_cli': about['__cli_name__'],
-                'pkg_name': PACKAGE_NAME.lower()
-            }
-        ]
-    },
+    description=about['__package_name__'].replace('_', ' ').title(),
+    entry_points=about['__entry_points__'],
     install_requires=about['__requires__'],
     license=about['__license__'],
     long_description=about['__desc__'],
-    name=PACKAGE_NAME,
+    name=about['__package_name__'],
     package_dir={"": "src"},
     packages=find_packages(where="src", exclude=['tests', 'tests.*']),
     platforms='any',
     scripts=about['__scripts__'],
     tests_require=['pytest'],
-    url='http://github.com/%s/%s' % (about['__author__'].replace(' ', '-'), PACKAGE_NAME),
+    url=about['__url__'],
     version=about['__version__'],
     zip_safe=False
 )
--- src/nscollector/__about__.py
+++ src/nscollector/__about__.py
@@ -4,6 +4,7 @@ __all__ = [
 ]
 
 __author__ = 'Luke Powers'
+__author_email__ = 'luke-powers@users.noreply.github.com'
 # For more classifiers, see http://goo.gl/zZQaZ
 __classifiers__ = [
     'Development Status :: 3 - Alpha',
@@ -16,15 +17,22 @@ __classifiers__ = [
     'Private :: Do Not Upload'
     # Do not allow this package to be uploaded to pypi.python.org
 ]
-__cli_name__ = 'ns-collector'
-__desc__ \
-    = '''Tool to scrape Nation States for stats'''
+__desc__ = '''Tool to scrape Nation States for stats'''
+__entry_points__ = {
+    'console_scripts':
+    [
+        'ns-collector=nscollector.__main__:main',
+    ]
+}
 __license__ = 'Apache Software License 2.0'
 __package_exclude__ = ['tests']
+__package_name__ = 'NSCollector'
 __requires__ = [
+    'pip>=9.0',
     'beautifulsoup',
     'django',
     'requests',
     ]
 __scripts__ = []
+__url__ = 'http://github.com/%s/%s' % (__author__.replace(' ', '-'), __package_name__)
 __version__ = '0.0.1'
