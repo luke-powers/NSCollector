a8d1e61b018367413aea334b349c540a8296afc6
Luke Powers <luke@openx.com>
2018-05-10 18:47:41 -0700

    removed needless _strip_words from gatherers; updated tests

--- src/nscollector/gatherers.py
+++ src/nscollector/gatherers.py
@@ -7,33 +7,22 @@ from bs4 import BeautifulSoup as soup
 NS = "https://www.nationstates.net"
 
 
-def _strip_word(bs_obj, word):
-    txt = bs_obj.getText()
-    if word in txt:
-        txt = txt.split(word)[1]
-    return txt
-
-
-def _gather_current_census(session):
+def _clean_census_objects(html_objects):
     '''Gather current census from website.
 
     '''
-    bs = soup(session.get('%s/page=list_nations?censusid=0' % NS).text, 'lxml')
     census_titles = [
-        (int(c.get('value')), _strip_word(c, 'Next: '))
-        for c in bs.findAll('select', {'name': 'censusid'})[0].findAll('option')
+        (int(c.get('value')), c.getText().replace('Next: ', '')) for c in html_objects
     ]
     census = sorted(census_titles, key=lambda x: x[1])
     return census
 
 
-def _issues_exist(session, nation):
-    '''Return number of issues if there are issues for the nation else False.
+def _issues_exist(issues_html):
+    '''Return number of issues if there are issues else False.
 
     '''
-    bs = soup(session.get('%s/nation=%s' % (NS, nation)).text, 'lxml')
-    issues = bs.findAll('div', {'id': 'notificationnumber-issues'})
-    return issues[0].getText() if issues else False
+    return issues_html[0].getText() if issues_html else False
 
 
 def process_issues(session, nation):
@@ -41,12 +30,28 @@ def process_issues(session, nation):
     Encountered Changes and the attribute weights.
 
     '''
-    if _issues_exist(session, nation):
-        print('issues exist')
+    bs = soup(session.get('%s/nation=%s' % (NS, nation)).text, 'lxml')
+    if _issues_exist(bs.findAll('div', {'id': 'notificationnumber-issues'})):
+        bs = soup(session.get('%s/page=dilemmas' % NS).text, 'lxml')
+        print(
+            list(
+                zip(
+                    [obj.getText() for obj in bs.findAll('div', {'class': 'dpaper4'})],
+                    [obj['href'] for obj in bs.findAll('a', {'class': 'dillistnpaper silentlink'})]
+                )
+            )
+        )
 
 
 def update_issue_census(session, _):
     '''Update the stored list of issue census in the database.
 
     '''
-    print([x[1] for x in _gather_current_census(session)])
+    bs = soup(session.get('%s/page=list_nations?censusid=0' % NS).text, 'lxml')
+    print(
+        [
+            x[1] for x in _clean_census_objects(
+                bs.findAll('select', {'name': 'censusid'})[0].findAll('option')
+            )
+        ]
+    )
--- tests/test_gatherers.py
+++ tests/test_gatherers.py
@@ -1,8 +1,18 @@
-from unittest.mock import Mock
 from nscollector import gatherers
+from unittest.mock import Mock
 
 
-def test_strip_word():
-    bs_obj = Mock(getText=Mock(side_effect=['Test1: foo', 'Test2: foo']))
-    assert gatherers._strip_word(bs_obj, 'Test1: ') == 'foo'
-    assert gatherers._strip_word(bs_obj, 'Test1: ') == 'Test2: foo'
+def test_clean_census_objects():
+    assert gatherers._clean_census_objects(
+        [
+            Mock(get=Mock(return_value=42), getText=Mock(return_value='forty-two')),
+            Mock(get=Mock(return_value=43), getText=Mock(return_value='forty-three'))
+        ]
+    ) == [(43, 'forty-three'), (42, 'forty-two')]  # Alfabetical sorting based on t[1]
+
+
+def test_issues_exist():
+    mock_html_obj = Mock(getText=Mock(return_value='42'))
+    assert gatherers._issues_exist([mock_html_obj]) == '42'
+    mock_html_obj = Mock(getText=Mock(return_value=''))
+    assert not gatherers._issues_exist([mock_html_obj])
